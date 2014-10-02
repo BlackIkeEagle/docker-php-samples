68ea582dda5d3a0e075901d4b2a7a8cf17c755fc
BlackEagle <ike.devolder@gmail.com>
2014-10-02 23:45:02 +0200

    owncloud-fpm run :: link port to other ip
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- docker/owncloud-fpm/run.sh
+++ docker/owncloud-fpm/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -v /srv/owncloud:/srv/owncloud -p 127.0.0.1:9003:9000 sample/owncloud-fpm
-#docker run -v /srv/owncloud:/srv/owncloud -p 127.0.0.1:9003:9000 -i -t sample/owncloud-fpm /bin/bash
+docker run -v /srv/owncloud:/srv/owncloud -p 192.168.1.1:9003:9000 sample/owncloud-fpm
