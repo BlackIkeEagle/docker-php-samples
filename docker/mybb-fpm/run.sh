926defe15514d2166b3f65fe77da7827ff909e65
BlackEagle <ike.devolder@gmail.com>
2014-10-02 23:44:41 +0200

    mybb-fpm run :: link port to other ip
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- docker/mybb-fpm/run.sh
+++ docker/mybb-fpm/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -v /srv/mybb:/srv/mybb -p 127.0.0.1:9002:9000 sample/mybb-fpm
-#docker run -v /srv/mybb:/srv/mybb -p 127.0.0.1:9002:9000 -i -t sample/mybb-fpm /bin/bash
+docker run -v /srv/mybb:/srv/mybb -p 192.168.1.1:9002:9000 sample/mybb-fpm
